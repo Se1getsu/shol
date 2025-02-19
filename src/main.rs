22f00d76b8cab0caa63c058f8efb3346e8d60cf4
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2025-02-19 11:00:38 +0900

    feat: プログラムはファイルから読み込むように変更

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,31 @@ pub mod lexer;
 lalrpop_mod!(pub shol);
 
 fn main() {
-    let input = "%print\n12345\n";
-    let mut lexer = lexer::Lexer::new(input);
+    // コマンドライン引数
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <input_file>", args[0]);
+        std::process::exit(1);
+    }
+    let input_file = &args[1];
+
+    // 入力ファイルの読み込み
+    let input = std::fs::read_to_string(input_file)
+        .expect("Failed to read input file");
+
+    // 構文解析の都合上, 入力の末尾には改行が必要
+    let input = {
+        let mut s = input;
+        if !s.ends_with('\n') {
+            s.push('\n');
+        }
+        s
+    };
+
+    // AST 生成
+    let mut lexer = lexer::Lexer::new(&input);
     let parser = shol::ProgramParser::new();
     let ast = parser.parse(&mut lexer);
+
     println!("{:?}", ast);
 }
