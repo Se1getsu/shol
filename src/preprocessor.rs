5ed6bf412a4defca5ba3e95f5adad7c1e5e4868a
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2025-02-23 15:14:05 +0900

    feat: リソースで"をエスケープしてもしなくても良くした

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -7,6 +7,7 @@ pub fn preprocess(program: &str) -> String {
     program
 }
 
+/// プログラムの末尾に改行がない場合は追加する (構文解析の都合上)
 fn append_trailing_new_line(program: &str) -> String {
     let mut program = program.to_string();
     if !program.ends_with('\n') {
@@ -15,23 +16,24 @@ fn append_trailing_new_line(program: &str) -> String {
     program
 }
 
+/// ダブルクォートが省略された文字列リソース行をダブルクォートで囲む
 fn add_double_quote(program: &str) -> String {
     // 以下で始まる行はダブルクォートで囲まない
     let excluding_token = vec![
         r"\+", r"\-", r"\*", r"\/", r"\%",
-        r"\.", r"\|",
+        r"\.", r"\|", "\"",
         r"[0-9]",
     ];
 
-    // 除外トークンのパターンを作成
-    let exclude_pattern = format!(r"^(?:{})", excluding_token.join("|"));
-    let exclude_re = Regex::new(&exclude_pattern).unwrap();
-
-    // 行全体にマッチするパターン
+    // 行全体にマッチするパターン: lsp, content, rsp に分割
     let line_pattern = r"(?m)^(?P<lsp>\s*)(?P<content>.*\S)(?P<rsp>\s*$)";
     let line_re = Regex::new(line_pattern).unwrap();
 
-    // 各行をチェックして必要な場合のみダブルクォートで囲む
+    // content がこれにマッチした場合は除外するパターン
+    let exclude_pattern = format!(r"^(?:{})", excluding_token.join("|"));
+    let exclude_re = Regex::new(&exclude_pattern).unwrap();
+
+    // 各行をチェック
     line_re.replace_all(program, |caps: &regex::Captures| {
         let lsp = &caps["lsp"];
         let content = &caps["content"];
@@ -40,7 +42,18 @@ fn add_double_quote(program: &str) -> String {
         if exclude_re.is_match(content) {
             format!("{}{}{}", lsp, content, rsp)
         } else {
-            format!("{}\"{}\"{}", lsp, content, rsp)
+            // エスケープ処理: " の直前の \ の数が偶数の場合のみエスケープする
+            let dq_re = Regex::new(r#"(?P<backslashes>\\*)""#).unwrap();
+            let escaped_content = dq_re.replace_all(content, |caps: &regex::Captures| {
+                let backslashes = &caps["backslashes"];
+                if backslashes.chars().count() % 2 == 0 {
+                    format!(r#"{}\""#, backslashes)
+                } else {
+                    format!(r#"{}""#, backslashes)
+                }
+            });
+            // ダブルクォートで囲む
+            format!(r#"{}"{}"{}"#, lsp, escaped_content, rsp)
         }
     }).to_string()
 }
