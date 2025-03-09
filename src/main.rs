157dc66e15a1b66fad89dd19cef660ff27cc8c72
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2025-03-09 19:44:34 +0900

    refactor: sys::process::exit の除去や Config の構造などをリファクタ

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, io::Write};
+use std::{env, io::Write, fs::File, process::ExitCode};
 use lalrpop_util::lalrpop_mod;
 use getopts::Options;
 use tempfile::Builder;
@@ -11,18 +11,25 @@ pub mod semantics;
 pub mod code_generator;
 lalrpop_mod!(pub shol);
 
-/// コマンドライン引数をパースして得られる設定
 struct Config {
+    /// ソースファイル名
     src_file: String,
-    save_temps: bool,
-    out_rs_file: String,
-    out_shi_file: String,
+    /// 中間生成ファイル名
+    shi_file: Option<String>,
+    /// 中間生成ファイル名
+    rs_file: String,
+    /// 実行ファイル名
     exe_file: String,
 }
 
-fn main() {
+fn main() -> ExitCode {
+    // コマンドライン引数のパース
     let args: Vec<String> = env::args().collect();
     let config = parse_args(args);
+    if let Err(e) = config {
+        return e;
+    }
+    let config = config.unwrap();
 
     // 入力ファイルの読み込み
     let program = std::fs::read_to_string(&config.src_file)
@@ -31,8 +38,9 @@ fn main() {
     // プリプロセス
     println!("[*] Preprocessing...");
     let program = preprocessor::preprocess(&program);
-    if config.save_temps {
-        let mut out_file = std::fs::File::create(&config.out_shi_file)
+
+    if let Some(path) = config.shi_file {
+        let mut out_file = File::create(&path)
             .expect("Failed to create output file");
         out_file.write_all(program.as_bytes())
             .expect("Failed to write program to file");
@@ -52,7 +60,7 @@ fn main() {
     println!("{{\"AST\":{:?}}}", ast);
 
     // 出力ファイルを開く
-    let mut out_file = std::fs::File::create(&config.out_rs_file)
+    let mut out_file = File::create(&config.rs_file)
         .expect("Failed to create output file");
 
     // コード生成
@@ -62,22 +70,20 @@ fn main() {
 
     // コンパイル
     println!("\n[*] Compiling...");
-    compile_rs_file(&config);
-
-    // 中間生成ファイルの削除
-    if !config.save_temps {
-        std::fs::remove_file(&config.out_rs_file).expect("Failed to remove intermediate file");
+    if let Err(e) = compile_rs_file(&config.rs_file, &config.exe_file) {
+        return e;
     }
 
     println!("[*] Completed.");
+    ExitCode::SUCCESS
 }
 
-/// コンパイル
-fn compile_rs_file(config: &Config) {
+/// rustc でコンパイル
+fn compile_rs_file(rs_file: &str, exe_file: &str) -> Result<(), ExitCode> {
     let status = std::process::Command::new("rustc")
-        .arg(&config.out_rs_file)
+        .arg(rs_file)
         .arg("-o")
-        .arg(&config.exe_file)
+        .arg(exe_file)
         .arg("--crate-name") // クレート名に使用できないファイル名の場合に必要
         .arg("shol_generated")
         .status()
@@ -85,15 +91,14 @@ fn compile_rs_file(config: &Config) {
 
     if !status.success() {
         eprintln!("rustc returned with non-zero exit code");
-        if !config.save_temps {
-            std::fs::remove_file(&config.out_rs_file).ok();
-        }
-        std::process::exit(1);
+        Err(ExitCode::FAILURE)
+    } else {
+        Ok(())
     }
 }
 
 /// コマンドライン引数のパース
-fn parse_args(args: Vec<String>) -> Config {
+fn parse_args(args: Vec<String>) -> Result<Config, ExitCode> {
     fn print_usage(program: &str, opts: &Options) {
         let brief = format!("Usage: {} [options] <src_file>", program);
         print!("{}", opts.usage(&brief));
@@ -111,26 +116,26 @@ fn parse_args(args: Vec<String>) -> Config {
         Ok(m) => m,
         Err(f) => {
             eprintln!("{}", f);
-            std::process::exit(1);
+            return Err(ExitCode::FAILURE);
         }
     };
 
     if matches.opt_present("h") {
         print_usage(&program, &opts);
-        std::process::exit(0);
+        return Err(ExitCode::SUCCESS);
     }
 
     if matches.free.is_empty() {
         eprintln!("エラー: ソースファイルが指定されていません");
         print_usage(&program, &opts);
-        std::process::exit(1);
+        return Err(ExitCode::FAILURE);
     }
 
     let src_file = matches.free[0].clone();
     let save_temps = matches.opt_present("save-temps");
 
     // 出力ファイル名の設定
-    let out_rs_file = if save_temps {
+    let rs_file = if save_temps {
         format!("{}.rs", src_file.trim_end_matches(".shol"))
     } else {
         Builder::new()
@@ -142,18 +147,22 @@ fn parse_args(args: Vec<String>) -> Config {
             .into_owned()
     };
 
-    let out_shi_file = format!("{}.shi", src_file.trim_end_matches(".shol"));
+    let shi_file = if save_temps {
+        Some(format!("{}.shi", src_file.trim_end_matches(".shol")))
+    } else {
+        None
+    };
+
     let exe_file = if cfg!(target_os = "windows") {
         format!("{}.exe", src_file.trim_end_matches(".shol"))
     } else {
         src_file.trim_end_matches(".shol").to_string()
     };
 
-    Config {
+    Ok(Config {
         src_file,
-        save_temps,
-        out_rs_file,
-        out_shi_file,
+        shi_file,
+        rs_file,
         exe_file,
-    }
+    })
 }
