694b80ee5e7af41b0daeec8c20f8e40193e7f0a4
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2025-03-22 17:53:26 +0900

    feat: ヘッダを Shol のアイコンに

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -68,10 +68,10 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Shol',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Shol Logo',
+        src: 'img/logo.png',
       },
       items: [
         {
