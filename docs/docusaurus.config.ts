e918553b682606dd1b9f16b2d1d6d230a251816c
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2025-03-25 01:06:48 +0900

    delete: Blog 削除

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -67,7 +67,6 @@ const config: Config = {
           position: 'left',
           label: 'リファレンス',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -107,10 +106,6 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
